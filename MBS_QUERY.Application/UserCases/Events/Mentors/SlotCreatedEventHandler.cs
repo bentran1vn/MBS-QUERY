cb7c8791dede83fb9fe88418a65d6d3def1b9983
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-10-16 20:36:25 +0700

    Implement event handling for MentorSlotCreated
    Added necessary using directives in `SlotCreatedEventHandler.cs` and `MentorSlotConsumer.cs` to include required namespaces. Introduced new namespaces for better organization. Implemented `MentorSlotCreatedEventHandler` to handle `DomainEvent.MentorSlotCreated`, fetching and updating mentor slots in the repository. Added `MentorSlotConsumer` with an inner class `MentorSlotCreatedConsumer` to consume `DomainEvent.MentorSlotCreated` events using dependency injection.

---
+++ MBS_QUERY.Application/UserCases/Events/Mentors/SlotCreatedEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using MBS_CONTRACT.SHARE.Abstractions.Shared;
+using MBS_CONTRACT.SHARE.Services.Users;
+using MBS_QUERY.Domain.Abstractions.Repositories;
+using MBS_QUERY.Domain.Documents;
+using MongoDB.Bson;
+using static MBS_CONTRACT.SHARE.Abstractions.Shared.Result;
+
+namespace MBS_QUERY.Application.UserCases.Events.Mentors;
+
+public class MentorSlotCreatedEventHandler(IMongoRepository<MentorProjection> mongoRepository)
+    : MBS_CONTRACT.SHARE.Abstractions.Messages.ICommandHandler<DomainEvent.MentorSlotCreated>
+{
+    public async Task<Result> Handle(DomainEvent.MentorSlotCreated request, CancellationToken cancellationToken)
+    {
+        var mentor = await mongoRepository.FindOneAsync(x => x.DocumentId == request.MentorId);
+        if (mentor is null) 
+        {
+            return Failure(new Error("404", "Mentor not found"));
+        }
+        //insert slot into mentor
+
+        var slotProjection = request.Slots.Select(x => new SlotProjection()
+        {
+            Id=new ObjectId(),
+            DocumentId = x.Id,
+            MentorId = x.MentorId,
+            Date = x.Date,
+            StartTime = x.StartTime,
+            EndTime = x.EndTime,
+            IsOnline = x.IsOnline,
+            Note = x.Note,
+            Month = x.Month,
+            IsBook = x.IsBook,
+        }).ToList();
+        mentor.MentorSlots = slotProjection;
+        await mongoRepository.ReplaceOneAsync(mentor);
+        return Success();
+
+
+
+    }
+}
---
+++ MBS_QUERY.Infrastructure/Consumer/MessageBus/Consumers/Events/MentorSlotConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MBS_CONTRACT.SHARE.Services.Users;
+using MBS_QUERY.Domain.Abstractions.Repositories;
+using MBS_QUERY.Domain.Documents;
+using MBS_QUERY.Infrastructure.Consumer.Abstractions.Messages;
+using MediatR;
+
+namespace MBS_QUERY.Infrastructure.Consumer.MessageBus.Consumers.Events;
+
+public static class MentorSlotConsumer
+{
+    public class MentorSlotCreatedConsumer(ISender sender, IMongoRepository<EventProjection> repository)
+        : Consumer<DomainEvent.MentorSlotCreated>(sender, repository);
+}
