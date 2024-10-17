5678fdfd36b81ecb09feaf2386190f0d985bdce7
bentran1vn <tan182205@gmail.com>
2024-10-17 23:03:07 +0700

    Add Swagger Config Bearer

--- MBS-QUERY.API/DependencyInjection/Extensions/SwaggerExtensions.cs
+++ MBS-QUERY.API/DependencyInjection/Extensions/SwaggerExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using MBS_QUERY.API.DependencyInjection.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -9,8 +12,73 @@ public static class SwaggerExtensions
 {
     public static void AddSwaggerAPI(this IServiceCollection services)
     {
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme()
+            {
+                Description = @"JWT Authorization header using the Bearer scheme. 
+
+Enter 'Bearer' [space] and then your token in the text input below.
+
+Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+            
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        },
+                        Scheme = "oauth2",
+                        Name = JwtBearerDefaults.AuthenticationScheme,
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
+            
+            c.OperationFilter<SwaggerFormDataOperationFilter>();
+            
+            c.EnableAnnotations();
+            
+        });
         services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+    }
+    
+    public class SwaggerFormDataOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var formParameters = context.MethodInfo
+                .GetParameters()
+                .Where(p => p.GetCustomAttributes(true)
+                    .Any(attr => attr.GetType() == typeof(FromFormAttribute)))
+                .ToList();
+
+            if (formParameters.Any())
+            {
+                foreach (var param in formParameters)
+                {
+                    operation.RequestBody = new OpenApiRequestBody
+                    {
+                        Content = {
+                            ["multipart/form-data"] = new OpenApiMediaType
+                            {
+                                Schema = context.SchemaGenerator.GenerateSchema(param.ParameterType, context.SchemaRepository)
+                            }
+                        }
+                    };
+                }
+            }
+        }
     }
 
     public static void UseSwaggerAPI(this WebApplication app)
