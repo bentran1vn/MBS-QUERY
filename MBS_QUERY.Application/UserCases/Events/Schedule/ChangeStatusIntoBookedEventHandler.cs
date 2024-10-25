0dfbf53eb34d988eaaea883692a9717405649495
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-10-25 11:26:12 +0700

    Update

---
+++ MBS_QUERY.Application/UserCases/Events/Schedule/ChangeStatusIntoBookedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using MBS_CONTRACT.SHARE.Abstractions.Messages;
+using MBS_CONTRACT.SHARE.Abstractions.Shared;
+using MBS_CONTRACT.SHARE.Services.Slots;
+using MBS_QUERY.Domain.Abstractions.Repositories;
+using MBS_QUERY.Domain.Documents;
+
+
+namespace MBS_QUERY.Application.UserCases.Events.Schedule;
+public class ChangeStatusIntoBookedEventHandler : ICommandHandler<DomainEvent.ChangeSlotStatusInToBooked>
+{
+    private readonly IMongoRepository<SlotProjection> _mongoRepository;
+
+    public ChangeStatusIntoBookedEventHandler(IMongoRepository<SlotProjection> mongoRepository)
+    {
+        _mongoRepository = mongoRepository;
+    }
+
+    public async Task<Result> Handle(DomainEvent.ChangeSlotStatusInToBooked request,
+        CancellationToken cancellationToken)
+    {
+        var slot = await _mongoRepository.FindOneAsync(x => x.SlotId == request.SlotId);
+        slot.IsBook = true;
+        await _mongoRepository.ReplaceOneAsync(slot);
+        return Result.Success();
+    }
+}
---
+++ MBS_QUERY.Infrastructure/Consumer/MessageBus/Consumers/Events/ScheduleConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MBS_CONTRACT.SHARE.Services.Slots;
+using MBS_QUERY.Domain.Abstractions.Repositories;
+using MBS_QUERY.Domain.Documents;
+using MBS_QUERY.Infrastructure.Consumer.Abstractions.Messages;
+using MediatR;
+
+namespace MBS_QUERY.Infrastructure.Consumer.MessageBus.Consumers.Events;
+public static class ScheduleConsumer
+{
+    public class ChangeStatusIntoBookedConsumer(ISender sender, IMongoRepository<EventProjection> _mongoRepository)
+        : Consumer<DomainEvent.ChangeSlotStatusInToBooked>(sender, _mongoRepository);
+
+}
